cd92a607781012abd17fc97a7d59d4920acd9199
ashwin2200 <ashwincharath@gmail.com>
2022-06-24 20:36:43 -0700

    add search function, fix dashboard

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from modules import app, SQLAlchemy
 from models import Job, db, User
 import traceback # ! DEBUG
 from flask import jsonify, render_template, request, abort, redirect
-import datetime, json
-import os
+import datetime, json, os, markdown
 
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 
@@ -25,25 +24,24 @@ def new_job(model):
         db.session.commit()
     except Exception as e:
         print(f"Error adding job: {e}")
-        with open('db_creation.log', 'w') as f:
+        with open('db_creation.log', 'a') as f:
             f.write(traceback.format_exc())
 
 def modify_job(id, new_model):
     try:
         job = Job.query.filter_by(id=id).first()
-        job.posted_on = new_model.posted_on
-        job.expires_on = new_model.expires_on
-        job.title = new_model.title
-        job.summary = new_model.summary
-        job.description = new_model.description
-        job.company = new_model.company
-        job.location = new_model.location
-        job.url = new_model.url
-        job.position = new_model.position
+        job.posted_on = new_model.posted_on if new_model.posted_on != None else job.posted_on
+        job.expires_on = new_model.expires_on if new_model.expires_on != "" else job.expires_on
+        job.title = new_model.title if new_model.title != "" else job.title
+        job.description = new_model.description if new_model.description != "" else job.description
+        job.summary = new_model.summary if new_model.summary != "" else job.summary
+        job.location = new_model.location if new_model.location != "" else job.location
+        job.company = new_model.company if new_model.company != "" else job.company
+        job.url = new_model.url if new_model.url != "" else job.url
         db.session.commit()
     except Exception as e:
         print(f"Error modifying job: {e}")
-        with open('db_creation.log', 'w') as f:
+        with open('db_creation.log', 'a') as f:
             f.write(traceback.format_exc())
 
 def delete_job(id):
@@ -53,7 +51,7 @@ def delete_job(id):
         db.session.commit()
     except Exception as e:
         print(f"Error deleting job: {e}")
-        with open('db_creation.log', 'w') as f:
+        with open('db_creation.log', ) as f:
             f.write(traceback.format_exc())
         return abort(404)
 
@@ -61,7 +59,7 @@ def delete_job(id):
 def new():
     if request.method =="POST":
         new_posting = Job(
-            posted_on = datetime.datetime.now().strftime("%d/%m/%y"),
+            posted_on = datetime.datetime.now(),
             expires_on = datetime.datetime.strptime(request.form['expires_on'], '%Y-%m-%d'),
             title = request.form['title'],
             summary = request.form['summary'],
@@ -75,7 +73,7 @@ def new():
             new_job(new_posting)
         except Exception as e:
             print(f"Error adding job: {e}")
-            with open('db_creation.log', 'w') as f:
+            with open('db_creation.log', ) as f:
                 f.write(traceback.format_exc())
             abort(500)
 
@@ -85,18 +83,18 @@ def new():
 
 @app.route('/edit/', methods=['POST'])
 def edit():
-    posted_on = datetime.datetime.strptime(request.form['posted_on'], '%Y-%m-%d')
-    expires_on = datetime.datetime.strptime(request.form['expires_on'], '%Y-%m-%d')
-    title = request.form['title']
-    summary = request.form['summary']
-    description = request.form['description']
-    company = request.form['company']
-    location = request.form['location']
-    url = request.form['url']
-    position = request.form['position']
-    new_posting = Job(posted_on, expires_on, title, summary, description, company, location, url)
+    fposted_on = "" if request.form['posted_on'] == None else datetime.datetime.strptime(request.form['posted_on'], '%Y-%m-%d')
+    fexpires_on = "" if request.form['expires_on'] == None else datetime.datetime.strptime(request.form['expires_on'], '%Y-%m-%d')
+    ftitle = "" if request.form['title'] == None else request.form['title']
+    fsummary = "" if request.form['summary'] == None else request.form['summary']
+    fdescription =   "" if request.form['description'] == None else request.form['description']
+    fcompany = "" if request.form['company'] == None else request.form['company']
+    flocation = "" if request.form['location'] == None else request.form['location']
+    furl = "" if request.form['url'] == None else request.form['url']
+    fposition = "" if request.form['position'] == None else request.form['position']
+    new_posting = Job(posted_on=fposted_on, position=fposition, expires_on=fexpires_on, title=ftitle, summary=fsummary, description=fdescription, company=fcompany, location=flocation, url=furl)
     modify_job(request.form['id'], new_posting)
-    return ""
+    return redirect('/')
 
 @app.route('/delete/', methods=['POST'])
 def delete():
@@ -107,22 +105,10 @@ def search(args):
     if request.method == "GET":
         search_results = Job.query.filter(Job.title.like(f"%{args}%")).all()
         if len(search_results) > 0:
-            results = []
+            results = ""
             for result in search_results:
-                r = {
-                    "id": result.id,
-                    "posted_on": result.posted_on,
-                    "expires_on": result.expires_on,
-                    "title": result.title,
-                    "summary": result.summary,
-                    "description": result.description,
-                    "company": result.company,
-                    "location": result.location,
-                    "url": result.url,
-                    "position": result.position
-                }
-            results.append(r)
-            return jsonify(results)
+                results+=f"<li class='w-full my-2'><a class='rounded-lg p-3 bg-slate-50 dark:bg-gray-600' href='/job/{result.id}'>{result.title}</a></li>"
+            return results
         else:
             return jsonify([])
     else:
@@ -130,12 +116,13 @@ def search(args):
 
 @app.route('/', methods=['GET'])
 def index():
-    jobs = Job.query.all()
-    filtered_jobs = [job for job in jobs if not has_posting_expired(job)]
-    for job in filtered_jobs:
-        job.posted_on = job.posted_on.strftime("%d/%m/%y")
+    if request.method == "GET":
+        jobs = Job.query.all()
+        filtered_jobs = [job for job in jobs if not has_posting_expired(job)]
+        for job in filtered_jobs:
+            job.posted_on = job.posted_on.strftime("%d/%m/%y")
 
-    return render_template('index.html', jobs=filtered_jobs, config=site_data)
+        return render_template('index.html', jobs=filtered_jobs, config=site_data)
 
 @app.route('/job/<int:id>/', methods=['GET'])
 def job(id):
@@ -146,6 +133,8 @@ def job(id):
         return abort(410)
     job.posted_on = job.posted_on.strftime('%B, %d, %Y')
     job.expires_on = job.expires_on.strftime('%B, %d, %Y')
+    job.description = markdown.markdown(job.description)
+
     return render_template('job.html', job=job, config=site_data)
 
 @app.route('/purge/<string:password>/', methods=['GET'])
--- start_proj.bat
+++ start_proj.bat
@@ -1,3 +1,4 @@
 source dev/Scripts/activate
 export FLASK_ENV=development
+export ACCESS_TOKEN=Ashwin123
 python -m flask run
